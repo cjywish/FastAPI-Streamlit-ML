e3c0789dfd44c4e47f702193ad55836f8ddf4314
unknown <cjywish@naver.com>
2026-01-17 15:56:09 +0900

    title added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
+# predict iris 
 st.set_page_config(page_title="Iris 분류 및 로그 시스템", layout="wide")
 
 st.title("🌸 Iris 꽃 분류 서비스 (ML + FastAPI + DB)")
